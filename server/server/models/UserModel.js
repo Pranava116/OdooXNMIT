734fb907fff569c95eb702e860075e4894cf77db
Pranava116 <pranavagrao116@gmail.com>
2025-09-06 08:40:23 +0530

    New Commit

--- server/server/models/UserModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const UserModel = new Schema({
-    username: String,
-    password: String
-})
-const User = mongoose.model("User", UserModel)
-export default User;
